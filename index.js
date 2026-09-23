c5d990dcedf7a116694f22ac246c289e51e3bdad
Matthew Scheuermann <mattscheuer03@gmail.com>
2026-09-23 14:41:05 -0500

    added functions and logic for sortOne and sortAll

--- index.js
+++ index.js
@@ -8,5 +8,29 @@ let evensArray = [];
 // Take user input for number bank
 function addToBank(number) {
   bankArray.push(number);
-  render();
+  //   render();
 }
+
+// Sort1 number
+function sortOne() {
+  const number = bankArray.shift();
+  if (number % 2 === 0) {
+    evensArray.push(number);
+  } else {
+    oddsArray.push(number);
+  }
+  // render();
+}
+
+function sortAll() {
+  while (bankArray.length > 0) {
+    sortOne();
+  }
+}
+
+// testing
+// addToBank(8);
+// addToBank(5);
+// addToBank(2);
+// sortAll();
+// console.log(oddsArray, evensArray);
