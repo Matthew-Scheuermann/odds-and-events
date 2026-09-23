5790121c354077d9d9cf36b4d756c1a193e31c15
Matthew Scheuermann <mattscheuer03@gmail.com>
2026-09-23 14:00:22 -0500

    built blank containers and first function

---
+++ index.js
@@ -0,0 +1,12 @@
+// Number Bank App
+
+// containers
+let bankArray = [];
+let oddsArray = [];
+let evensArray = [];
+
+// Take user input for number bank
+function addToBank(number) {
+  bankArray.push(number);
+  render();
+}
